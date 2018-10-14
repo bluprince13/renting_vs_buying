c0dd4ad611a69cff16924e38cb11275d1e8898f3
bluprince13 <vipinajayakumar@icloud.com>
2018-10-14 13:20:56 +0100

    info and units added

--- src/components/form/InputElement.js
+++ src/components/form/InputElement.js
@@ -4,24 +4,73 @@ import styled from 'styled-components';
 
 const StyledInput = styled.div`
 	display: flex;
-	flex-direction: row
-`
+	flex-flow: row;
+`;
 
 const StyledSlider = styled.div`
 	flex: 2;
-`
+`;
 
 const StyledText = styled.div`
-	flex: 1;
+	flex: 1.5;
 	margin: 1rem;
-`
+`;
 
-const InputElement = (props) => {
-	const { label, name, min, max, type } = props.props
+const ToolTipText = styled.span`
+	visibility: hidden;
+	width: 12rem;
+	background-color: black;
+	color: #fff;
+	text-align: center;
+	padding: 0.5rem 1rem;
+	border-radius: 0.6rem;
+
+	position: absolute;
+	z-index: 1;
+	top: 50%;
+	left: 50%;
+	transform: translate(+10%, -50%);
+
+	&:after {
+		content: ' ';
+		position: absolute;
+		top: 50%;
+		right: 100%; /* To the left of the tooltip */
+		margin-top: -0.5rem;
+		border-width: 0.5rem;
+		border-style: solid;
+		border-color: transparent black transparent transparent;
+	}
+`;
+
+const Info = styled.div`
+	margin-left: 0.5rem;
+	display: inline-block;
+	position: relative
+
+	&:hover {
+		${ToolTipText} {
+			visibility: visible;
+		}
+	}
+`;
+
+const InputElement = props => {
+	const { label, name, min, max, type, info, unit } = props.props;
 	return (
 		<StyledInput>
 			<StyledSlider>
-				<label htmlFor={name} style={{display: 'block'}}>{label}</label>
+				<div>
+					<label htmlFor={name}>{label}</label>
+					{info ? (
+						<Info>
+							<i className="fas fa-info-circle" />
+							<ToolTipText>{info}</ToolTipText>
+						</Info>
+					) : (
+						''
+					)}
+				</div>
 				<Field
 					name={name}
 					component="input"
@@ -36,11 +85,13 @@ const InputElement = (props) => {
 					name={name}
 					component="input"
 					type={'text'}
+					size={8}
 					parse={value => Number(value)}
 				/>
+				<label style={{marginLeft: '0.5rem'}}>{unit}</label>
 			</StyledText>
 		</StyledInput>
 	);
 };
 
-export default InputElement
+export default InputElement;
--- src/components/form/fields.js
+++ src/components/form/fields.js
@@ -5,7 +5,8 @@ export default [
 		min: 50000,
 		max: 1000000,
 		value: 200000,
-		type: 'range'
+		type: 'range',
+		info: 'The home price excluding any costs incurred in purchasing it.'
 	},
 	{
 		label: 'Mortgage annual interest',
@@ -13,15 +14,19 @@ export default [
 		min: 0,
 		max: 25,
 		value: 5,
-		type: 'range'
+		type: 'range',
+		info: 'The annual interest rate for the mortgage over your term, as quoted by your lender.',
+		unit: '%'
 	},
 	{
-		label: 'Down payment as percentage of home price',
+		label: 'Down payment',
 		name: 'downPaymentPercentage',
 		min: 0,
 		max: 100,
 		value: 10,
-		type: 'range'
+		type: 'range',
+		info: 'The amount of money, as a percentage of home price, you will put towards the purchase of your home upfront that will not be covered by your mortgage.',
+		unit: '%'
 	},
 	{
 		label: 'Amortization',
@@ -29,23 +34,29 @@ export default [
 		min: 0,
 		max: 40,
 		value: 20,
-		type: 'range'
-    },
-    {
+		type: 'range',
+		info: 'The total length of time you will take to pay off your mortgage.',
+		unit: 'years'
+	},
+	{
 		label: 'Stamp duty',
 		name: 'stampDutyPercentage',
 		min: 0,
 		max: 20,
 		value: 0,
-		type: 'range'
-    },
-    {
+		type: 'range',
+		info: '',
+		unit: '%'
+	},
+	{
 		label: 'Other home purchase costs',
 		name: 'homePurchaseCosts',
 		min: 0,
 		max: 10000,
 		value: 3000,
-		type: 'range'
+		type: 'range',
+		info: '',
+		unit: ''
 	},
 	{
 		label: 'Annual home value appreciation',
@@ -53,46 +64,58 @@ export default [
 		min: -5,
 		max: 15,
 		value: 2,
-		type: 'range'
-    },
-    {
+		type: 'range',
+		info: '',
+		unit: '%'
+	},
+	{
 		label: 'Cost of selling home',
 		name: 'homeSellingFeesPercentage',
 		min: 0,
 		max: 10,
 		value: 5,
-		type: 'range'
-    },
-    {
-        label: 'Monthly rent',
+		type: 'range',
+		info: '',
+		unit: '%'
+	},
+	{
+		label: 'Monthly rent',
 		name: 'rent',
 		min: 50,
 		max: 1500,
 		value: 400,
-		type: 'range'
-    },
-    {
-        label: 'Annual change in rent',
+		type: 'range',
+		info: '',
+		unit: ''
+	},
+	{
+		label: 'Annual change in rent',
 		name: 'rentChange',
 		min: 0,
 		max: 15,
 		value: 2,
-		type: 'range'
-    },
-    {
-        label: 'Expected annual investment return',
+		type: 'range',
+		info: '',
+		unit: '%'
+	},
+	{
+		label: 'Expected annual investment return',
 		name: 'investmentReturn',
 		min: 0,
 		max: 20,
 		value: 5,
-		type: 'range'
-    },
-    {
-        label: 'Inflation',
+		type: 'range',
+		info: '',
+		unit: '%'
+	},
+	{
+		label: 'Inflation',
 		name: 'inflation',
 		min: -5,
 		max: 10,
 		value: 3,
-		type: 'range'
-    },
+		type: 'range',
+		info: '',
+		unit: '%'
+	}
 ];

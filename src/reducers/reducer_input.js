13aee8eb2d10b68de453fdd056f5aecaae634818
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2018-10-31 21:53:34 +0000

    formatting

--- src/reducers/reducer_input.js
+++ src/reducers/reducer_input.js
@@ -1,10 +1,10 @@
 import { CHANGE_INPUT } from '../actions/index'
 import { initialValues } from '../functions'
 
-export default (state = initialValues, action) =>  {
+export default (state = initialValues, action) => {
     switch (action.type) {
         case CHANGE_INPUT:
-            return {...state, ...action.payload};
+            return { ...state, ...action.payload };
     }
 
     return state;

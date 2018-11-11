6f3c60b2a8411e1246a222987597871a080f6a24
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2018-11-11 13:22:20 +0000

    to get rid of warning

--- src/reducers/reducerInput.js
+++ src/reducers/reducerInput.js
@@ -4,6 +4,8 @@ export default (state = {}, action) => {
     switch (action.type) {
         case CHANGE_INPUT:
             return { ...state, ...action.payload };
+        default:
+            return state;
     }
 
     return state;

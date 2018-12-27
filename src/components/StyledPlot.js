bf29aee742d18aad0a79e3f7f47f2ac16532e442
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2018-12-27 21:56:38 +0000

    plot height adjusted

--- src/components/StyledPlot.js
+++ src/components/StyledPlot.js
@@ -4,7 +4,7 @@ import Plot from 'react-plotly.js';
 class StyledPlot extends React.Component {
 	render() {
 		return (
-            <Plot {...this.props} style={{height: "60vh"}}
+            <Plot {...this.props} style={{height: "65vh"}}
                 config={{
                     responsive: true
                 }}

c918fc2c776545d4f16b6b5ed21c193d53dae2f7
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2019-04-28 15:03:36 +0400

    Download table as csv

---
+++ src/components/tables/Table.js
@@ -0,0 +1,120 @@
+import React from "react";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import d3 from "d3";
+import { CSVLink } from "react-csv";
+import styled from "styled-components";
+
+import Footer from "../Footer";
+
+const StyledButton = styled.button`
+	-webkit-transition-duration: 0.4s;
+	transition-duration: 0.4s;
+
+	display: block;
+	margin: auto;
+
+	width: 25%;
+	height: 5rem;
+
+	background-color: white;
+	color: black;
+	border: 0.2rem solid #4caf50;
+
+	border-radius: 0.5rem;
+
+	&:hover {
+		background-color: #4caf50;
+		color: white;
+	}
+
+	&:focus {
+		outline: none;
+	}
+`;
+
+const format = number => {
+	if (Number.isInteger(number)) {
+		return number;
+	}
+	return d3.format(".3s")(number);
+};
+const style = {
+	textAlign: "center"
+};
+const Cell = ({ value }) => <div>{format(value)}</div>;
+
+class Table extends React.Component {
+	constructor(props) {
+		super(props);
+		this.download = this.download.bind(this);
+		this.state = {
+			dataToDownload: []
+		};
+	}
+
+	download(event) {
+		const { columns } = this.props;
+
+		const currentRecords = this.reactTable.getResolvedState().sortedData;
+		var data_to_download = [];
+		for (var index = 0; index < currentRecords.length; index++) {
+			let record_to_download = {};
+			for (var colIndex = 0; colIndex < columns.length; colIndex++) {
+				record_to_download[columns[colIndex].Header] =
+					currentRecords[index][columns[colIndex].accessor];
+			}
+			data_to_download.push(record_to_download);
+		}
+		this.setState({ dataToDownload: data_to_download }, () => {
+			this.csvLink.link.click();
+		});
+	}
+
+	render() {
+		const { columnNames, data, title } = this.props;
+
+		const columns = columnNames.map(columnName => ({
+			Header: columnName,
+			accessor: columnName,
+			style,
+			Cell
+		}));
+
+		return (
+			<div>
+				<h2>{title}</h2>
+				<ReactTable
+					ref={r => (this.reactTable = r)}
+					data={data}
+					columns={columns}
+					defaultPageSize={10}
+					className="-striped -highlight"
+				/>
+				<Footer
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "center",
+						overflowY: "auto"
+					}}
+				>
+					<StyledButton onClick={this.download}>
+						Download CSV
+					</StyledButton>
+					<div>
+						<CSVLink
+							data={this.state.dataToDownload}
+							filename="data.csv"
+							className="hidden"
+							ref={r => (this.csvLink = r)}
+							target="_blank"
+						/>
+					</div>
+				</Footer>
+			</div>
+		);
+	}
+}
+
+export default Table;
--- src/components/tables/TableBuyScenario.js
+++ src/components/tables/TableBuyScenario.js
@@ -1,49 +1,44 @@
-import React from 'react';
-import ReactTable from "react-table";
-import "react-table/react-table.css";
-import d3 from 'd3';
+import React from "react";
+import Table from "./Table";
 
-const format = d3.format(".5s");
-const style = {
-	textAlign: 'center'
-}
-const Cell = ({ value }) => <div>{format(value)}</div>
-
+const columnNames = ["year", "homeValueEachYear", "debtEachYear", "homeMaintenanceCostsEachYear", "rentIncomeEachYear", "homeSellingFeesEachYear", "loanPaymentEachYear", "cashFlowIn", "cashFlowOut", "cashFlowNet", "cashFlowNetPosCumulative", "netWorthBuyPos", "netWorthBuyNeg", "netWorthBuy", "netWorthBuyPV"]
+const title = "Buy scenario"
 class TableBuyScenario extends React.Component {
 	render() {
-		const { buyScenarioOutputs } = this.props
-		const { 
+		const { buyScenarioOutputs } = this.props;
+		const {
 			totalTime,
-	
+
 			homeValueEachYear,
 			debtEachYear,
-	
+
 			homeMaintenanceCostsEachYear,
 			rentIncomeEachYear,
-	
+
 			homeSellingFeesEachYear,
-	
+
 			loanPaymentEachYear,
-	
+
 			cashFlowIn,
 			cashFlowOut,
 			cashFlowNet,
 			cashFlowNetPosCumulative,
-	
+
 			netWorthBuyPos,
 			netWorthBuyNeg,
 			netWorthBuy,
-			netWorthBuyPV,
-		 } = buyScenarioOutputs
+			netWorthBuyPV
+		} = buyScenarioOutputs;
 
-		const data = totalTime.map((year) => {
+		const data = totalTime.map(year => {
 			return {
 				year: year,
 
 				homeValueEachYear: homeValueEachYear[year],
 				debtEachYear: debtEachYear[year],
 
-				homeMaintenanceCostsEachYear: homeMaintenanceCostsEachYear[year],
+				homeMaintenanceCostsEachYear:
+					homeMaintenanceCostsEachYear[year],
 				rentIncomeEachYear: rentIncomeEachYear[year],
 
 				homeSellingFeesEachYear: homeSellingFeesEachYear[year],
@@ -57,110 +52,16 @@ class TableBuyScenario extends React.Component {
 				netWorthBuyPos: netWorthBuyPos[year],
 				netWorthBuyNeg: netWorthBuyNeg[year],
 				netWorthBuy: netWorthBuy[year],
-				netWorthBuyPV: netWorthBuyPV[year],
-			}
-		})
+				netWorthBuyPV: netWorthBuyPV[year]
+			};
+		});
 
 		return (
-			<div>
-				<h2>Buy scenario</h2>
-				<ReactTable
-					data={data}
-					columns={[
-						{
-							Header: "Year",
-							accessor: "year",
-							style,
-						},
-						{
-							Header: "homeValueEachYear",
-							accessor: "homeValueEachYear",
-							style,
-							Cell
-						},
-						{
-							Header: "debtEachYear",
-							accessor: "debtEachYear",
-							style,
-							Cell
-						},
-						{
-							Header: "homeMaintenanceCostsEachYear",
-							accessor: "homeMaintenanceCostsEachYear",
-							style,
-							Cell
-						},
-						{
-							Header: "rentIncomeEachYear",
-							accessor: "rentIncomeEachYear",
-							style,
-							Cell
-						},
-						{
-							Header: "homeSellingFeesEachYear",
-							accessor: "homeSellingFeesEachYear",
-							style,
-							Cell
-						},
-						{
-							Header: "loanPaymentEachYear",
-							accessor: "loanPaymentEachYear",
-							style,
-							Cell
-						},
-						{
-							Header: "cashFlowIn",
-							accessor: "cashFlowIn",
-							style,
-							Cell
-						},
-						{
-							Header: "cashFlowOut",
-							accessor: "cashFlowOut",
-							style,
-							Cell
-						},
-						{
-							Header: "cashFlowNet",
-							accessor: "cashFlowNet",
-							style,
-							Cell
-						},
-						{
-							Header: "cashFlowNetPosCumulative",
-							accessor: "cashFlowNetPosCumulative",
-							style,
-							Cell
-						},
-						{
-							Header: "netWorthBuyPos",
-							accessor: "netWorthBuyPos",
-							style,
-							Cell
-						},
-						{
-							Header: "netWorthBuyNeg",
-							accessor: "netWorthBuyNeg",
-							style,
-							Cell
-						},
-						{
-							Header: "netWorthBuy",
-							accessor: "netWorthBuy",
-							style,
-							Cell
-						},
-						{
-							Header: "netWorthBuyPV",
-							accessor: "netWorthBuyPV",
-							style,
-							Cell
-						},
-					]}
-					defaultPageSize={10}
-					className="-striped -highlight"
-				/>
-			</div>
+			<Table
+				data={data}
+				title={title}
+				columnNames={columnNames}
+			/>
 		);
 	}
 }

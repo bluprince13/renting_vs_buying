d31b5f74ca74f73facbe865199974948a6c6b8c6
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2019-04-29 18:54:50 +0100

    cash flow footer

--- src/components/plots/PlotBuyCashFlow.js
+++ src/components/plots/PlotBuyCashFlow.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import * as math from 'mathjs';
-import StyledPlot from '../StyledPlot';
-import Footer from '../Footer';
+import React from "react";
+import * as math from "mathjs";
+import StyledPlot from "../StyledPlot";
+import Footer from "../Footer";
+import Annot from "../Annot";
+import { format2, format3 } from "../../functions/helpers";
 
 class PlotBuyCashFlow extends React.Component {
 	render() {
-		const { buyScenarioOutputs } = this.props
-		const { rentIncomeEachYear } = buyScenarioOutputs
-		let { 
+		const { buyScenarioOutputs } = this.props;
+		let {
 			initialCosts,
 			homeMaintenanceCostsEachYear,
+			rentIncomeEachYear,
 			loanPaymentEachYear
-		} = buyScenarioOutputs
-		let { time, cashFlowIn, cashFlowOut, cashFlowNet } = buyScenarioOutputs
+		} = buyScenarioOutputs;
+		let { time, cashFlowIn, cashFlowOut, cashFlowNet } = buyScenarioOutputs;
 
-		initialCosts = math.multiply(-1, initialCosts)
-		homeMaintenanceCostsEachYear = math.multiply(-1, homeMaintenanceCostsEachYear)
-		loanPaymentEachYear = math.multiply(-1, loanPaymentEachYear)
-		cashFlowOut = math.multiply(-1, cashFlowOut)
+		initialCosts = -initialCosts;
+		homeMaintenanceCostsEachYear = math.multiply(
+			-1,
+			homeMaintenanceCostsEachYear
+		);
+		loanPaymentEachYear = math.multiply(-1, loanPaymentEachYear);
+		cashFlowOut = math.multiply(-1, cashFlowOut);
 
 		return (
 			<div>
@@ -26,75 +31,85 @@ class PlotBuyCashFlow extends React.Component {
 						{
 							x: time,
 							y: rentIncomeEachYear,
-							name: 'Rent income each year',
-							type: 'scatter',
-							stackgroup: 'zero'
+							name: "Rent income each year",
+							type: "scatter",
+							stackgroup: "zero"
 						},
 						{
 							x: time,
 							y: loanPaymentEachYear,
-							name: 'Loan payment',
-							type: 'scatter',
-							stackgroup: 'one'
+							name: "Loan payment",
+							type: "scatter",
+							stackgroup: "one"
 						},
 						{
 							x: time,
 							y: homeMaintenanceCostsEachYear,
-							name: 'Home maintenance costs',
-							type: 'scatter',
-							stackgroup: 'one'
+							name: "Home maintenance costs",
+							type: "scatter",
+							stackgroup: "one"
 						},
 						{
 							x: [0, 1],
 							y: [initialCosts, 0],
-							name: 'Initial costs',
-							type: 'scatter',
-							stackgroup: 'one'
+							name: "Initial costs",
+							type: "scatter",
+							stackgroup: "one"
 						},
 						{
 							x: time,
 							y: cashFlowIn,
-							name: 'Cashflow In',
-							type: 'scatter',
+							name: "Cashflow In",
+							type: "scatter"
 						},
 						{
 							x: time,
 							y: cashFlowOut,
-							name: 'Cashflow Out',
-							type: 'scatter',
+							name: "Cashflow Out",
+							type: "scatter"
 						},
 						{
 							x: time,
 							y: cashFlowNet,
-							name: 'Cashflow Net',
-							type: 'scatter',
-							mode: 'lines',
+							name: "Cashflow Net",
+							type: "scatter",
+							mode: "lines",
 							line: {
-								color: 'red',
+								color: "red",
 								width: 4
 							}
 						}
 					]}
 					layout={{
-						title: 'Buy scenario: Cashflow',
+						title: "Buy scenario: Cashflow",
 						xaxis: {
-							title: 'Time (years)',
-							hoverformat: '.0f',
+							title: "Time (years)",
+							hoverformat: ".0f",
 							showgrid: true,
 							zeroline: true,
-							showline: true,
+							showline: true
 						},
 						yaxis: {
-							title: 'Nominal value',
-							tickformat: '.2s',
-							hoverformat: '.2s',
+							title: "Nominal value",
+							tickformat: ".2s",
+							hoverformat: ".2s",
 							showgrid: true,
 							zeroline: true,
-							showline: true,
+							showline: true
 						}
 					}}
 				/>
 				<Footer>
+					<p>
+						Cash flow is important to consider when planning to buy
+						a house. First you have the initial costs of {" "}
+						<Annot>{format2(-initialCosts)}</Annot>. Following this,
+						you have loan payments until the end of your mortgage
+						term. Home maintenance costs are also accounted for, but
+						note that in reality home maintenance costs will not be
+						evenly distributed. You could also have some rental
+						income each year.
+					</p>
 				</Footer>
 			</div>
 		);

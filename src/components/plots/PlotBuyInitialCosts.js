1c90f0a62135b6f219da4360f961baf40c8bb8b9
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2019-04-29 14:40:08 +0400

    formatting

--- src/components/plots/PlotBuyInitialCosts.js
+++ src/components/plots/PlotBuyInitialCosts.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import StyledPlot from '../StyledPlot';
-import Footer from '../Footer';
-import Annot from '../Annot';
-import { format2, format3 } from '../../functions/helpers';
+import React from "react";
+import StyledPlot from "../StyledPlot";
+import Footer from "../Footer";
+import Annot from "../Annot";
+import { format2, format3 } from "../../functions/helpers";
 
 class PlotBuyInitialCosts extends React.Component {
 	render() {
-		const { buyScenarioOutputs } = this.props
-		const { downPayment, stampDuty, homePurchaseCosts, initialCosts } = buyScenarioOutputs
+		const { buyScenarioOutputs } = this.props;
+		const {
+			downPayment,
+			stampDuty,
+			homePurchaseCosts,
+			initialCosts
+		} = buyScenarioOutputs;
 
 		return (
 			<div>
@@ -16,34 +21,35 @@ class PlotBuyInitialCosts extends React.Component {
 						{
 							values: [downPayment, stampDuty, homePurchaseCosts],
 							labels: [
-							"Down payment",
-							"Stamp duty",
-							"Other home purchase costs",
+								"Down payment",
+								"Stamp duty",
+								"Other home purchase costs"
 							],
-							hole: .4,
+							hole: 0.4,
 							type: "pie"
-						},
-					]}
-					layout={
-						{
-							title: "Buy scenario: Initial costs",
-							annotations: [
-								{
-									font: {
-										size: 10
-									},
-									showarrow: false,
-									text: `${initialCosts}`,
-									x: 0.5,
-									y: 0.5
-								}
-							]
 						}
-					}
+					]}
+					layout={{
+						title: "Buy scenario: Initial costs",
+						annotations: [
+							{
+								font: {
+									size: 10
+								},
+								showarrow: false,
+								text: `${initialCosts}`,
+								x: 0.5,
+								y: 0.5
+							}
+						]
+					}}
 				/>
 				<Footer>
 					<p>
-						The initial costs of <Annot>{format2(initialCosts)}</ Annot> in the buy scenario make for the initial investment in the rent scenario.
+						The initial costs of{" "}
+						<Annot>{format2(initialCosts)}</Annot> in the buy
+						scenario make for the initial investment in the rent
+						scenario.
 					</p>
 				</Footer>
 			</div>
@@ -51,4 +57,4 @@ class PlotBuyInitialCosts extends React.Component {
 	}
 }
 
-export default PlotBuyInitialCosts
+export default PlotBuyInitialCosts;
